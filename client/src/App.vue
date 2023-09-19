bd5dbf1cc093f68026f79e4c8ddfc08428cf470b
Anna Kucherenko <Annakucherenko1997@gmail.com>
2023-09-19 22:02:28 +0200

    navbar added, first api call added

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,76 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <NavBar :currentDay="currentDay"/>
+  <ErrorSnackBar v-model:snackbar="snackbar" :text="text" />
+  <!-- Render a <div> for each element in this.stations -->
+  <div v-for="(station, index) in stations" :key="index">
+    <!--display station data here -->
+    {{ station.title }}
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import ErrorSnackBar from './components/ErrorSnackBar.vue';
+import NavBar from './components/NavBar.vue';
+  import { getStations } from './services/api'; 
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    name: 'App',
+    components: {
+    NavBar,
+    ErrorSnackBar
+},
+    created() {
+      this.fetchStations()
+      .then((response) => {
+        console.log(response);
+        if (response === null || !response?.status || !response.result) {
+          console.log("shpuld show snackbar");
+          this.toggleSnackbar();
+          return;
+        }
+        this.setStations(response.result);
+        console.log("fetched: ", response);
+      });
+    },
+    data() {
+      return {
+        snackbar: false,
+        text: `Ooops, something went wrong. Failed to fetch data, please refresh the page.`,
+        currentDay: 'dnes',
+        stations: [],
+      };
+    },
+    mounted() {
+      window.addEventListener('scroll', this.updateCurrentDay);
+    },
+    beforeUnmount() {
+      window.removeEventListener('scroll', this.updateCurrentDay);
+    },
+    
+    methods: {
+      updateCurrentDay() {
+        const scrollPosition = window.scrollY;
+        if (scrollPosition < 500) {
+          this.currentDay = 'dnes'; 
+        } else if (scrollPosition < 1000) {
+          this.currentDay = 'včera'; 
+        } else if (scrollPosition < 1500) {
+          this.currentDay = 'zajtra'; 
+        } else {
+          this.currentDay = 'pozajtra'; 
+        }
+      },
+      toggleSnackbar() {
+        this.snackbar = !this.snackbar;
+      },
+      setStations(stations) {
+        this.stations = stations;
+      },
+      async fetchStations() {
+        return await getStations();
+      }
+    },
+  };
 </script>
 
 <style>
@@ -19,8 +78,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ client/src/components/NavBar.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="navbar">
+    <div class="logo">
+      <!-- Logo on the left -->
+      <img src="./../assets/systemomega-logo.png" alt="Logo" />
+    </div>
+    <div>
+      <div class="borderForImg">
+        <div class="images">
+          <!-- Images on the right -->
+          <div class="currentDay">hello {{ currentDay }}</div>
+      <img src="./../assets/jojka.png" alt="logo jojka" class="jojka"/>
+      <img src="./../assets/markíza.png" alt="logo markiza" class="markiza"/>
+      <img src="./../assets/jednotka.png" alt="logo jednotka" class="jednotka"/>
+    </div>
+   </div>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  props: {
+    currentDay: {
+      type: String,
+      default: 'dnes', 
+    },
+  }
+};
+</script>
+
+<style scoped>
+/* Add styles for the navbar */
+.navbar {
+  position: sticky;
+  top: 0;
+  display: flex;
+  /* justify-content: space-between; */
+  flex-direction: column;
+  /* align-items: center; */
+  padding-top: 20px;
+  border: 1px;
+  height: 9vh;
+  border-bottom: 2px solid #333;
+}
+
+/* Style for the logo */
+.logo img {
+  margin-left: 3%;
+  height: 40px;
+  margin-right: 10px;
+}
+.currentDay {
+  background-color: black;
+  height: 30px;
+  color: white;
+  /* z-index: 99; */
+  width: 60px;
+  border-radius: 10%;
+  font-size: 10px;
+}
+/* Style for the images on the right */
+.images {
+  /* display: flex;
+  align-items: flex-end;  */
+width: 100%;
+display: flex;
+align-items: center;
+justify-content: center;  
+gap: 10%
+/* background-color: black; */
+}
+
+.jojka {
+  height: 70px;
+  width: 60px
+}
+.jednotka {
+  height: 70px;
+  width: 70px
+}
+.markiza {
+  background-color: rgb(36, 36, 177);
+  width: 130px;
+  padding: 5px;
+  border-radius: 10px;
+  
+}
+/* Style for the border in the middle */
+.borderForImg {
+ margin-top: -30px;
+}
+</style>
+
